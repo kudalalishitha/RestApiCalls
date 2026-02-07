bad6f08f1a3de0254a4787b6ab8a51c1585a6e97
Lishitha Kudala <kudalalishitha@gmail.com>
2026-02-07 22:41:59 +0530

    UserDTO for request body handling

---
+++ src/main/java/org/example/dto/UserDTO.java
@@ -0,0 +1,26 @@
+package org.example.dto;
+
+public class UserDTO {
+
+    private String firstName;
+    private String lastName;
+
+    public UserDTO() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
